bc4d02cca1d3f7d4ead1c754669164a2777f20ea
zzwh5 <195917428@qq.com>
2020-10-15 08:57:32 +0800

    新增时间选择器

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 引入vant的组件
 import "./until/components"
 Vue.config.productionTip = false;
+// 引入时间插件 一、 vueHashCalendar
+// import vueHashCalendar from 'vue-hash-calendar'
+// // 引入组件CSS样式
+// import 'vue-hash-calendar/lib/vue-hash-calendar.css'
+// // 注册组件库
+// Vue.use(vueHashCalendar)
+
+// 时间组件 二、 
+import Calendar from 'vue-mobile-calendar'
+ 
+Vue.use(Calendar)
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <van-button type="primary">主要按钮</van-button>
+    <van-button type="primary" @click.native="show = true">选择日期</van-button>
+    <!-- <vue-hash-calendar :visible="true" model="dialog"></vue-hash-calendar> -->
+    <calendar :show.sync="show" @change="changeDate"> </calendar>
   </div>
 </template>
 <script>
 import { apiAddress } from "@/request/api";
+// 时间解析
+import moment from "moment";
 import axios from "axios";
 export default {
   data() {
-    return {};
+    return {
+      show: false
+    };
   },
   created() {
     // console.log(apiAddress);
@@ -21,7 +27,14 @@ export default {
         console.log(err);
       });
   },
-  methods: {}
+  methods: {
+    // 确定选择的日期
+    changeDate(e) {
+      console.log(e);
+      console.log(moment(e.$d).format("MMMM Do YYYY, h:mm:ss a"));
+      this.show = false;
+    }
+  }
 };
 </script>
 <style scoped lang="scss">
